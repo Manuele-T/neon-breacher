c6005972e050aafbf19352cc8551f452155cfaf5
Manuele-T <manuele.tacchetti@gmail.com>
2025-12-08 13:43:45 +0000

    Github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     return {
+      base: "/neon-breacher/",
       server: {
         port: 3000,
         host: '0.0.0.0',
